9892f9a5bb5f957c87fdf02211585a370e0869d2
이윤형 <lyh12qwaszx@gmail.com>
2024-01-31 17:58:05 +0900

    fix: '2차원 캐러셀 그리드 형태 오류 수정'
    기존에 상하 좌우로 돌렸을 때 안에 요소들이 나오지 않는 문제 발생하여
    해당 부분 해결, 하지만 그리드 형태로 나오게 하는데는 수정 필요
    
    Fixes: #S10P12C101-341

--- cookip/frontend/src/components/recipe/RowCarousel.vue
+++ cookip/frontend/src/components/recipe/RowCarousel.vue
@@ -1,28 +1,49 @@
 <template>
-  <VueSlickCarousel v-bind="settings">
-    <div v-for="(recipe_list, idx) in recipe_category" :key="idx">
-      <ColCarousel :recipe-list="recipe_list" />
-    </div>
-    <v-btn @click="showNext()">show me the next</v-btn>
-  </VueSlickCarousel>
+  <Carousel
+    ref="rowCarousel"
+    :itemsToShow="3.95"
+    :wrapAround="true"
+    :transition="500"
+    style="height: 1000px"
+  >
+    <Slide
+      class="test"
+      v-for="(recipe_list, slide) in recipe_category"
+      :key="slide"
+      style="height: 1000px"
+    >
+      <div class="carousel__item">
+        <ColCarousel :recipe-list="recipe_list" />
+      </div>
+    </Slide>
 
-  <FlipCard />
+    <template #addons>
+      <Pagination />
+    </template>
+  </Carousel>
+
+  <div>
+    <button @click="nextpage()">Next</button>
+    <input type="number" min="0" max="3" v-model="currentSlide" />
+    <button @click="prevpage()">Prev</button>
+  </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { Carousel, Pagination, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
 import ColCarousel from "./ColCarousel.vue";
-import FlipCard from "./FlipCard.vue";
 
-const settings = ref({
-  centerMode: true,
-  centerPadding: "20px",
-  focusOnSelect: true,
-  infinite: true,
-  slidesToShow: 3,
-  speed: 500,
-  arrows: true,
-});
+const rowCarousel = ref(null);
+
+const nextpage = () => {
+  rowCarousel.value.next();
+};
+
+const prevpage = () => {
+  rowCarousel.value.prev();
+};
 
 const recipe_category = ref([
   [{ name: "1" }, { name: "2" }, { name: "3" }, { name: "4" }],
@@ -30,7 +51,51 @@ const recipe_category = ref([
   [{ name: "9" }, { name: "10" }, { name: "11" }, { name: "12" }],
   [{ name: "13" }, { name: "14" }, { name: "15" }, { name: "16" }],
 ]);
-
 </script>
 
-<style scoped></style>
+<style scoped>
+.test {
+}
+.colcarousel {
+}
+
+.carousel__slide {
+  padding: 5px;
+}
+
+.carousel__viewport {
+  perspective: 2000px;
+}
+
+.carousel__track {
+  transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+  transition: 0.5s;
+}
+
+.carousel__slide {
+  opacity: 0.9;
+  transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active ~ .carousel__slide {
+  transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+  opacity: 1;
+  transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+  opacity: 1;
+  transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+  opacity: 1;
+  transform: rotateY(0) scale(1.1);
+}
+</style>
