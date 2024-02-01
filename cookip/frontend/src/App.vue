edc6b1ef2cacef36fdd3e17f52df0b02947674a9
이윤형 <lyh12qwaszx@gmail.com>
2024-02-01 17:27:53 +0900

    fix: '2차원 캐러셀 그리드 형태로 구'
    기존에 상하 좌우 이동은 되지만 그리드가 규칙적이지 못하고 수직방향의
    요소들이 겹쳐서 보이는 문제들을 해결
    
    Resolves: #S10P12C101-341
    Related to: #S10P12C101-331

--- cookip/frontend/src/App.vue
+++ cookip/frontend/src/App.vue
@@ -3,22 +3,24 @@
     <div>
       <h1>hello, APP.vue</h1>
     </div>
-    <RouterLink :to="{name:'main'}">main</RouterLink> |
-    <RouterLink :to="{name:'member'}">member</RouterLink> |
-    <RouterLink :to="{name:'home'}">home</RouterLink> |
-    <RouterLink :to="{name:'my-profile'}">my-profile</RouterLink> |
+    <RouterLink :to="{ name: 'main' }">main</RouterLink> |
+    <RouterLink :to="{ name: 'member' }">member</RouterLink> |
+    <RouterLink :to="{ name: 'home' }">home</RouterLink> |
+    <RouterLink :to="{ name: 'my-profile' }">my-profile</RouterLink> |
     <div class="screen">
       <RouterView />
     </div>
-</div>
+  </div>
 </template>
 
 <script setup>
-import { RouterView, RouterLink } from 'vue-router';
-
+import { RouterView, RouterLink } from "vue-router";
 </script>
 
 <style scoped>
-
+.screen {
+  width: 1920px;
+  height: 1080px;
+}
 
 </style>
--- cookip/frontend/src/views/RecipeView.vue
+++ cookip/frontend/src/views/RecipeView.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-    <h1>여기는 검색된 결과를 보여주는 페이지 입니다.</h1>
+  <div class="recipe-view">
+    <!-- <div class="recipe-view-header">
+      <h1>여기는 검색된 결과를 보여주는 페이지 입니다.</h1>
+    </div> -->
+    <div class="carousel-wrapper">
+      <RowCarousel />
+    </div>
   </div>
-
-  <RowCarousel />
 </template>
 
 <script setup>
 import RowCarousel from "@/components/recipe/RowCarousel.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+.recipe-view {
+  height: 100%;
+  width: 100%;
+}
+
+/* .recipe-view-header {
+  height: 10%;
+  text-align: center;
+} */
+
+.carousel-wrapper {
+  height: 100%;
+  width: 100%;
+}
+</style>
