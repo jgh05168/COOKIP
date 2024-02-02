f40e63e05de54527439fb138e8a710bece27fc8c
maruck3 <maruck3@naver.com>
2024-02-02 15:36:42 +0900

    reuse

--- cookip/frontend/src/store/recipe.js
+++ cookip/frontend/src/store/recipe.js
@@ -1,13 +1,22 @@
 import { defineStore } from 'pinia'
 // import { ref, computed } from 'vue'
 // import axios from 'axios'
-
+import accountService from '@/store/mvpApi';
+import { ref, onBeforeMount } from "vue";
 
 export const useRecipeStore = defineStore('recipe', () => {
+  const recipes = ref([]);
+  const error = ref("");
+  
+  onBeforeMount(async () => {
+    try {
+      const recipeData = await accountService.getUserRecipe();
+      recipes.value = recipeData;
+    } catch (err) {
+      error.value = err.message;
+    }
+  });
 
-    
-
-
-    return {}
+    return { recipes }
   }, { persist: true })
   
