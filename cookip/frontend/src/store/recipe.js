d500dc2d053ac8e41937bd86c3401a350a271aa7
이윤형 <lyh12qwaszx@gmail.com>
2024-02-08 14:10:47 +0900

    fix: '캐로셀 에서 레시피 상세페이지 가기 구현'

--- cookip/frontend/src/store/recipe.js
+++ cookip/frontend/src/store/recipe.js
@@ -15,7 +15,6 @@ export const useRecipeStore = defineStore('recipe', () => {
     {id:2, title:"실시간 인기 음식" , img:"popularity.png"}, // 실시간 인기 음식(좋아요 순)
     {id:3, title:"사용자 맞춤" , img:"profile.png"}, // 유통기한 적게 남은 순
     {id:4, title:"오늘의 추천" , img:"today.png"}, // 
-    {id:5, title:"조리시간 별 추천" , img:"refrigerator.png"}, // 
   ])
 
   // 카텍고리 별로 레시피 리스트 만들기
@@ -43,11 +42,6 @@ export const useRecipeStore = defineStore('recipe', () => {
   );
   })
 
-  const category_5 = computed(() => {
-    return  Array.from({ length: Math.ceil(recipes.value.length / 5) }, (value, index) =>
-    recipes.value.slice(index * 5, index * 5 + 5)
-  );
-  })
  
   // 추천 카테고리 별로 데이터 쪼개기
   const recommend_list = ref([
@@ -55,7 +49,6 @@ export const useRecipeStore = defineStore('recipe', () => {
     {category : 2, recipe_list: category_2},
     {category : 3, recipe_list: category_3},
     {category : 4, recipe_list: category_4},
-    {category : 5, recipe_list: category_5},
   ])
 
   const selected_category = ref(0)
