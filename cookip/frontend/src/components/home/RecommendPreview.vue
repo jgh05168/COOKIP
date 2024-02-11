8facf9a8f1bc8dc38c4ac67b80b996d24e2b09a6
선상범 <gksgusrhks12@naver.com>
2024-02-11 22:09:04 +0900

    0211커밋

--- cookip/frontend/src/components/home/RecommendPreview.vue
+++ cookip/frontend/src/components/home/RecommendPreview.vue
@@ -14,6 +14,7 @@
 import { watch, defineProps } from "vue"; // import reactive and onMounted from vue
 import { useRecipeStore } from "@/store/recipe";
 
+
 const recipeStore = useRecipeStore();
 
 const props = defineProps({
