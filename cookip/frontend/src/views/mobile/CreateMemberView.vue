689f3517a9aaf9251a008277f90a76d9da66c7f4
김건오 <dns7816@naver.com>
2024-02-14 11:31:12 +0900

    survey css kgo 0214

--- cookip/frontend/src/views/mobile/CreateMemberView.vue
+++ cookip/frontend/src/views/mobile/CreateMemberView.vue
@@ -122,7 +122,6 @@
 </script>
 
 <style scoped>
-  @media only screen and (max-width: 360px) and (max-height: 780px) {
     body {
       margin: 0;
       padding: 0; /* 기본 padding도 제거합니다. */
@@ -152,7 +151,7 @@
     }
     
     .survey-item {
-      margin-bottom: 1rem;
+      margin-bottom: 15px;
     }
     
     button {
@@ -190,6 +189,5 @@
     .submit-button:hover {
       background-color: #3E2723;
     }
-  }
 </style>
   
